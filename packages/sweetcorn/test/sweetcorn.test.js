0b00343c936f1b16ff2ccd802fbd39af936616a1
Chris Swithinbank <swithinbank@gmail.com>
2025-12-16 22:51:36 +0100

    Add e2e tests of `sweetcorn()` function

--- packages/sweetcorn/test/sweetcorn.test.js
+++ packages/sweetcorn/test/sweetcorn.test.js
@@ -1,9 +1,12 @@
 /// <reference types="node" />
 
-import { describe, it } from 'node:test';
 import assert from 'node:assert';
-import { applyDiffusionKernel, applyThresholdMap } from '../dist/processors.js';
+import { describe, it } from 'node:test';
+import sharp from 'sharp';
+import sweetcorn from 'sweetcorn';
 import diffusionKernels from '../dist/diffusion-kernels.js';
+import { applyDiffusionKernel, applyThresholdMap } from '../dist/processors.js';
+import thresholdMaps from '../dist/threshold-maps.json' with { type: 'json' };
 
 describe('algorithms', () => {
 	describe('applyThresholdMapToPixels()', () => {
@@ -19,13 +22,19 @@ describe('algorithms', () => {
 			assert(pixels.every((pixel) => pixel === 0 || pixel === 255));
 		});
 
-		it('it supports maps with multiple rows', () => {
+		it('supports maps with multiple rows', () => {
 			const pixels = [60, 120, 180, 240];
 			applyThresholdMap(pixels, 2, [
 				[64, 184],
 				[92, 255],
 			]);
 			assert.deepEqual(pixels, [0, 0, 255, 0]);
+		});
+
+		it('dithers using the built-in bayer-2 threshold map', () => {
+			const pixels = [127, 127, 127, 127];
+			applyThresholdMap(pixels, 2, thresholdMaps['bayer-2']);
+			assert.deepEqual(pixels, [255, 0, 0, 255]);
 		});
 	});
 
@@ -44,3 +53,46 @@ describe('algorithms', () => {
 	});
 });
 
+describe('sweetcorn()', () => {
+	it('dithers an image using a built-in threshold map', async () => {
+		const image = sharp(Buffer.from([127, 127, 127, 127]), {
+			raw: { width: 2, height: 2, channels: 1 },
+		});
+		const dithered = await sweetcorn(image, { algorithm: 'bayer-2' });
+		const data = await dithered.toColourspace('b-w').raw().toBuffer();
+		// Note that this result differs from the direct applyThresholdMap() test above.
+		// This is because we gamma correct Sharp images from srgb to linear before processing.
+		// Our input pixel value of 127 is only 37 after this correction.
+		assert.deepEqual(Array.from(data), [255, 0, 0, 0]);
+	});
+	
+	it('dithers an image using a built-in diffusion kernel', async () => {
+		const image = sharp(Buffer.from([60, 120, 180, 250]), {
+			raw: { width: 2, height: 2, channels: 1 },
+		});
+		const dithered = await sweetcorn(image, { algorithm: 'simple-diffusion' });
+		const data = await dithered.toColourspace('b-w').raw().toBuffer();
+		// See note above about gamma correction.
+		assert.deepEqual(Array.from(data), [0, 0, 0, 255]);
+	});
+
+	it('dithers an image using a custom threshold map', async () => {
+		const customMap = [[0]];
+		const image = sharp(Buffer.from([60, 120, 180, 240]), {
+			raw: { width: 2, height: 2, channels: 1 },
+		});
+		const dithered = await sweetcorn(image, { thresholdMap: customMap });
+		const data = await dithered.toColourspace('b-w').raw().toBuffer();
+		assert.deepEqual(Array.from(data), [255, 255, 255, 255]);
+	});
+
+	it('dithers an image using a custom diffusion kernel', async () => {
+		const customKernel = [[0, 255]];
+		const image = sharp(Buffer.from([60, 120, 180, 240]), {
+			raw: { width: 2, height: 2, channels: 1 },
+		});
+		const dithered = await sweetcorn(image, { diffusionKernel: customKernel });
+		const data = await dithered.toColourspace('b-w').raw().toBuffer();
+		assert.deepEqual(Array.from(data), [0, 255, 0, 255]);
+	});
+});
