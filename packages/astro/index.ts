937e8561753a7a2f3bb6002734d5081af7e5c0d4
Chris Swithinbank <swithinbank@gmail.com>
2025-12-17 07:55:18 +0100

    Inject types for user-defined algorithms

--- packages/astro/index.ts
+++ packages/astro/index.ts
@@ -29,6 +29,29 @@ export default function sweetcornAstro<T extends string = never, D extends strin
 				const serviceUrl = new URL('./service.ts', import.meta.url);
 				addWatchFile(serviceUrl);
 			},
+
+			/**
+			 * Add the names of user-defined algorithms to the type for the `dither` prop.
+			 * These are merged into the `Sweetcorn.Astro` interface in `image-props.d.ts`.
+			 */
+			'astro:config:done'({ injectTypes }) {
+				const customAlgorithms = [
+					...Object.keys(config.customDiffusionKernels || {}),
+					...Object.keys(config.customThresholdMaps || {}),
+				];
+				if (customAlgorithms.length > 0) {
+					injectTypes({
+						filename: 'custom-algorithms.d.ts',
+						content:
+							'declare namespace Sweetcorn {\n' +
+							'	export interface Astro {\n' +
+							'		/** Custom dithering algorithms provided by the user. */\n' +
+							`		CustomAlgorithms: ${customAlgorithms.map((a) => JSON.stringify(a)).join(' | ')};\n` +
+							'	}\n' +
+							'}',
+					});
+				}
+			},
 		},
 	} satisfies AstroIntegration;
 }
